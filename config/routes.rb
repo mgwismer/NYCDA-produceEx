6a9e18da785efc29cd8b8fd7aa7783a50661a2a7
Eric Falchier <ericfalchier@gmail.com>
2017-01-17 14:58:14 -0500

    changed static page routes

--- config/routes.rb
+++ config/routes.rb
@@ -15,11 +15,11 @@ Rails.application.routes.draw do
 
   root 'static_pages#home'
 
-  get 'static_pages/about'
+  get '/about', to: "static_pages#about"
 
-  get 'static_pages/contact'
+  get '/contact', to: "static_pages#contact"
 
-  get 'static_pages/help'
+  get '/help', to: "static_pages#help"
 
   get 'products/index'
 
