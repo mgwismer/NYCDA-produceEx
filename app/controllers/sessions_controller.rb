cbd592bd2bc2091a40faccfa437d36ee2eeccd22
Margaret Wismer <margaretgwismer.gmail.com>
2017-01-17 13:53:49 -0500

    update account form need to test before close

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,12 @@
 class SessionsController < Devise::SessionsController
   
  protected
- 
+ def after_sign_out_path_for(resource)
+  'root'
+ end
 
  def after_sign_in_path_for(resource)
   '/profiles/show'
  end
- 
+
 end 
