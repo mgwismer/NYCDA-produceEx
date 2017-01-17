c9bd688bf8e4321c1917a48d74d6e2ffe26eb2d8
Eric Falchier <ericfalchier@gmail.com>
2017-01-17 11:58:40 -0500

    add styling to sign in and sign out pages, configured signout, you can now destroy session

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,7 @@
 class Product < ApplicationRecord
 	has_many :sellers, through: :markets
+
+	def self.search(search)
+  where("name LIKE ? OR plu LIKE ? OR category LIKE ?", "%#{search}%", "%#{search}%", "%#{search}%", "%#{search}%") 
+	end
 end
