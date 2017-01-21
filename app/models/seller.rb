1b451107c0e840332b30a26d62ef3641a9930f74
TheButlerDidIt <labutler@gmail.com>
2017-01-21 12:14:05 -0500

    attempts to add avatar for sellers

--- app/models/seller.rb
+++ app/models/seller.rb
@@ -6,6 +6,9 @@ class Seller < ApplicationRecord
   has_many :markets
   has_many :products, through: :markets
 
+  has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
+  validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
+
  def self.search(search)
   		where("name LIKE ? OR city LIKE ? OR zipcode LIKE ?", "%#{search}%", "%#{search}%", "%#{search}%") 
 	end
