1575d42f4f6a2df07527fd04107523b23f29e4b1
Margaret Wismer <margaretgwismer.gmail.com>
2017-01-21 14:31:24 -0500

    seller signout not working

--- app/controllers/buyer_profiles_controller.rb
+++ app/controllers/buyer_profiles_controller.rb
@@ -3,6 +3,14 @@ class BuyerProfilesController < ApplicationController
   end
 
   def show
+    if params[:id]
+      @buyer = Buyer.find(params[:id])
+    else
+      @buyer = current_buyer
+    end
+    if params[:search]
+      @reviews = Review.search(params[:search]).order("created_at DESC")
+    end
   end
 
   def new
