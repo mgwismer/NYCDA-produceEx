2810d9d3d733d374e70a00e79f18d7ab85c75c63
Margaret Wismer <margaretgwismer.gmail.com>
2017-01-22 16:12:50 -0500

    trying to create seller reviews but getting rollback transaction

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -6,9 +6,20 @@ class ReviewsController < ApplicationController
   end
 
   def new
+    @sellers = Seller.all
+    @review = Review.new
   end
 
   def create
+    @review = Review.new(review_params)
+    @review.seller_id = params[:seller_id]
+    @review.buyer_id = current_buyer.id
+    if @review.save
+      redirect_to buyer_profiles_show_path
+    else 
+      flash[:review_error] = "Comment Not Saved"
+      redirect_back(fallback_location: new_review_path)
+    end
   end
 
   def edit
@@ -19,4 +30,15 @@ class ReviewsController < ApplicationController
 
   def destroy
   end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:comment, :rating)
+  end
+
+  def set_review
+    @review = Review.find(params[:id])
+  end
+
 end
