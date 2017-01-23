5ce83ad8dfa74e74960440070b65147dbcb328b2
Margaret Wismer <margaretgwismer.gmail.com>
2017-01-23 09:42:37 -0500

    go to profile page after sign in

--- app/controllers/Sellers/sessions_controller.rb
+++ app/controllers/Sellers/sessions_controller.rb
@@ -1,6 +1,14 @@
 class Sellers::SessionsController < Devise::SessionsController
 # before_action :configure_sign_in_params, only: [:create]
+  
+ protected
+ def after_sign_out_path_for(resource)
+  '/'
+ end
 
+ def after_sign_in_path_for(resource)
+  '/profiles/show'
+ end
   # GET /resource/sign_in
   # def new
   #   super
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,12 +1,12 @@
 class SessionsController < Devise::SessionsController
   
- protected
- def after_sign_out_path_for(resource)
-  'root'
- end
+ # protected
+ # def after_sign_out_path_for(resource)
+ #  'root'
+ # end
 
- def after_sign_in_path_for(resource)
-  '/profiles/show'
- end
+ # def after_sign_in_path_for(resource)
+ #  '/profiles/show'
+ # end
 
 end 
